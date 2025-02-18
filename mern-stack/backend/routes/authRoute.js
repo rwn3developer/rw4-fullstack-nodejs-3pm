a0404c98aa2ff98914557fae22a3f0ce4cf27b80
rwn3developer <rwn3developer11@gmail.com>
2025-02-18 16:03:48 +0530

    role auth done

--- mern-stack/backend/routes/authRoute.js
+++ mern-stack/backend/routes/authRoute.js
@@ -2,8 +2,13 @@ const express = require('express');
 
 const routes = express.Router();
 
-const { loginUser } = require('../controllers/AuthController');
+const { loginUser, registerUser, dummyApi, allUser } = require('../controllers/AuthController');
+const { verifyToken, authorizeRole } = require('../middleware/Auth');
 
 routes.post('/login', loginUser)
+routes.post('/register', registerUser)
+routes.get('/dummyapi', verifyToken, dummyApi)
+routes.get('/adminalluser', verifyToken, authorizeRole, allUser)
+
 
 module.exports = routes;
