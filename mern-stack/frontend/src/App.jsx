d767c3630bf88c49edb9e81926f4ecdf23b3dd4b
rwn3developer <rwn3developer11@gmail.com>
2025-02-19 15:51:36 +0530

    api calling

--- mern-stack/frontend/src/App.jsx
+++ mern-stack/frontend/src/App.jsx
@@ -1,10 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route />
+        <Route path='/' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+
       </Routes>
     </BrowserRouter>
   )
